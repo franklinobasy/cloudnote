26472ad822557b42463f1accb4053ba9336f1068
Franklin Obasi <franklinobasy@gmail.com>
2022-05-07 18:19:34 +0100

    Added password to access db

--- server1/__init__.py
+++ server1/__init__.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://postgres:Frank6408@localhost:5432/cloudnote-db")
+
+password = input("Enter password")
+engine = create_engine(f"postgresql://postgres:{password}@localhost:5432/cloudnote-db")
 
 Session = sessionmaker(bind= engine)
 session = Session()
