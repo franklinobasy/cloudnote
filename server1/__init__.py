cad3011bca739440275c6f18c61ae5a28ec33473
Franklin Obasi <franklinobasy@gmail.com>
2022-05-07 18:23:45 +0100

    Updated README.md

--- server1/__init__.py
+++ server1/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-password = input("Enter password")
+password = input("Enter password: ")
 engine = create_engine(f"postgresql://postgres:{password}@localhost:5432/cloudnote-db")
 
 Session = sessionmaker(bind= engine)
