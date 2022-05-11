33088f6632f127186156b21f7729d67f920bdce7
Franklin Obasi <franklinobasy@gmail.com>
2022-05-12 00:36:39 +0100

    used heroku postgres database

--- server1/__init__.py
+++ server1/__init__.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from server1.credentials import PASSWORD
+from server1.credentials import username, password, host, port, db_name
 
-password = "*********"
-engine = create_engine(f"postgresql://postgres:{password}@localhost:5432/cloudnote-db")
-
+username = username
+password = password
+host = host
+port = port
+db_name = db_name
+engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{db_name}")
+#engine = create_engine(f"postgresql://xnmhjmjyyhyiwl:d97d5c5498331ac059f8cbf67197b4fc9f52e29379f883baa4717ec187bd3b9d@ec2-18-210-64-223.compute-1.amazonaws.com:5432/d4oobb9t3dkr3j")
 Session = sessionmaker(bind= engine)
 session = Session()
 
