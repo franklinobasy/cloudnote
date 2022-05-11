dff8d390946047d4891de50c94806b31cb7df3ca
Franklin Obasi <franklinobasy@gmail.com>
2022-05-12 00:40:11 +0100

    used heroku postgres database

--- server1/__init__.py
+++ server1/__init__.py
@@ -8,7 +8,7 @@ host = host
 port = port
 db_name = db_name
 engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{db_name}")
-#engine = create_engine(f"postgresql://xnmhjmjyyhyiwl:d97d5c5498331ac059f8cbf67197b4fc9f52e29379f883baa4717ec187bd3b9d@ec2-18-210-64-223.compute-1.amazonaws.com:5432/d4oobb9t3dkr3j")
+
 Session = sessionmaker(bind= engine)
 session = Session()
 
